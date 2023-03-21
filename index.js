21fe60acb617737cde6922553283ff1ab890a065
Marwanak00 <marwan.akouripro@gmail.com>
2023-03-21 16:54:32 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let age = 25 ;
+let nom = "Jean";
+let somme = 5 + 7;
+let chaine = "bonjour tout le monde";
+let maj = chaine.toUpperCase();
+let tableau =[1,3,7,9];
+let longueur = tableau.length;
+let objet = {nom:AKOURI , age:22};
+let nombre = 3.14 ;
+let result1 = nombre.toFixed();
+console.log(result1);
+let date = "21/03/2023";
+let newdate = date.getMonth();
+let tableau2 =["Paris","Agadir","Amsterdam","New York"];
+console.log(tableau2.join());
+
+
+
